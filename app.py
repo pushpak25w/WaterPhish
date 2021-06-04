7596f0841c25eacde0935a69f09110ffb1b4fbb5
prarthanachandak <69465478+prarthanachandak@users.noreply.github.com>
2021-06-04 20:30:35 +0530

    added warnings

--- app.py
+++ app.py
@@ -3,28 +3,26 @@ from features import phishing
 
 app=Flask(__name__)
 
-
-def phish():
+@app.route('/',methods=["GET","POST"])
+def index():
+	output = ""
+	warnings = []
 	if request.method == "POST":
 		url = request.form.get('url')
 
-		val = phishing(url)
-		val = int(val)
+		if(url==""):
+			return render_template('searchpage.html')
 
+		val, warnings = phishing(url)
+		val = int(val)
 		if val==1:
 			output = "Legitimate"
 		elif val==0:
 			output = "Suspicious"
 		else:
-			output = "Phishing!"
+			output = "Phishing"
 
-		return(output)
-
-@app.route('/', methods=["GET","POST"])
-
-def index():
-	output = phish()
-	return render_template('searchpage.html', output=output)
+	return render_template('searchpage.html', output = output, warnings = warnings)
 
 if __name__ == "__main__":
 	app.run(debug=True)
